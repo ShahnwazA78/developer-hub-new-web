4dd788b6188548ee8b0cef937a681ffb93781552
ShahnwazA78 <93584064+ShahnwazA78@users.noreply.github.com>
2022-10-04 15:21:04 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
             <input type="checkbox" id="showDrop" />
 
             <label htmlFor="showDrop" className="mobile-item">
-              <Link to="/courses"> Courses </Link>
+              <a href ="/courses"> Courses </a>
             </label>
             <ul className="drop-menu">
               <li>
@@ -49,7 +49,7 @@ function Navbar() {
             </a>
             <input type="checkbox" id="showDrop" />
             <label htmlFor="showDrop" className="mobile-item">
-              <Link to="/"> Segment </Link>
+              <a href="/segment"> Segment </a>
             </label>
             <ul className="drop-menu">
               <li>
@@ -72,7 +72,7 @@ function Navbar() {
             </a>
             <input type="checkbox" id="showDrop" />
             <label htmlFor="showDrop" className="mobile-item">
-              <Link to="/"> Partners </Link>
+              <a href="/partenrs"> Partners </a>
             </label>
             <ul className="drop-menu">
               <li>
@@ -101,7 +101,7 @@ function Navbar() {
             </a>
             <input type="checkbox" id="showMega" />
             <div htmlFor="showMega" className="mobile-item">
-              <Link to="/"> Mega Menu </Link>
+              <a href="/megamenu"> Mega Menu </a>
             </div>
             <div className="mega-box">
               <div className="content">
@@ -183,93 +183,3 @@ function Navbar() {
   );
 }
 export default Navbar;
-// import React, { useState } from 'react';
-// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-// import logo from '../../logo.svg';
-// import './navbar.css';
-
-// const Navbar = () => {
-//   const [toggleMenu, setToggleMenu] = useState(false);
-
-//   return (
-//     <div className="gpt3__navbar">
-//       <div className="gpt3__navbar-links">
-//         <div className="gpt3__navbar-links_logo">
-//           <img src={logo} />
-//         </div>
-//         <div className="gpt3__navbar-links_container">
-//           {/* <p><a href="#home">Courses</a></p> */}
-//           <div className="row">
-//             <div className="col-lg-6 mb-4">
-//               <h6 className="font-weight-bold text-uppercase">MegaMenu heading</h6>
-//               <ul className="list-unstyled">
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0">Unique Features</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Image Responsive</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Auto Carousel</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Newsletter Form</a></li>
-//               </ul>
-//             </div>
-//             <div className="col-lg-6 mb-4">
-//               <h6 className="font-weight-bold text-uppercase">MegaMenu heading</h6>
-//               <ul className="list-unstyled">
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Unique Features</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Image Responsive</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Auto Carousel</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Newsletter Form</a></li>
-//               </ul>
-//             </div>
-//             <div className="col-lg-6 mb-4">
-//               <h6 className="font-weight-bold text-uppercase">MegaMenu heading</h6>
-//               <ul className="list-unstyled">
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Unique Features</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Image Responsive</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Auto Carousel</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Newsletter Form</a></li>
-//               </ul>
-//             </div>
-//             <div className="col-lg-6 mb-4">
-//               <h6 className="font-weight-bold text-uppercase">MegaMenu heading</h6>
-//               <ul className="list-unstyled">
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Unique Features</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Image Responsive</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Auto Carousel</a></li>
-//                 <li className="nav-item"><a href="/" className="nav-link text-small pb-0 ">Newsletter Form</a></li>
-//               </ul>
-//             </div>
-//           </div>
-//           <p><a href="#wgpt3">Segment</a></p>
-//           <p><a href="#possibility">Partners</a></p>
-//           <p><a href="#features">About Us</a></p>
-//           <p><a href="#blog">Support</a></p>
-//         </div>
-//       </div>
-
-//       <div className="gpt3__navbar-sign">
-//         <p>Sign in</p>
-//         <button type="button">Sign up</button>
-//       </div>
-//       <div className="gpt3__navbar-menu">
-//         {toggleMenu
-//           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-//           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
-//         {toggleMenu && (
-//           <div className="gpt3__navbar-menu_container scale-up-center">
-//             <div className="gpt3__navbar-menu_container-links">
-//               <p><a href="#home">Courses</a></p>
-//               <p><a href="#wgpt3">Segment</a></p>
-//               <p><a href="#possibility">Partners</a></p>
-//               <p><a href="#features">About Us</a></p>
-//               <p><a href="#blog">Support</a></p>
-//             </div>
-//             <div className="gpt3__navbar-menu_container-links-sign">
-//               <p>Sign in</p>
-//               <button type="button">Sign up</button>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
